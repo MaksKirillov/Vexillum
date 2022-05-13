ad9b62e454e91973efb758cfcb3531d77465a1f2
Maxim Kirillov <maks_kirillov_2003@mail.ru>
2022-05-13 13:52:59 +0300

    Added launcher

---
+++ src/main/java/com/vex/vexillum/Launcher.java
@@ -0,0 +1,9 @@
+package com.vex.vexillum;
+
+import java.io.FileNotFoundException;
+
+public class Launcher {
+    public static void main(String[] args) throws FileNotFoundException {
+        VexApplication.main(args);
+    }
+}
